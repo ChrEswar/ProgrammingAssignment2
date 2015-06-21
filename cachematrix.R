1be0bf82c1d1092ec9f714075b9096938b450ac0
chrEswar <chrdseswar@gmail.com>
2015-06-21 14:20:24 -0400

    Inverse matrix with execution steps

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,75 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  Programming Assignment 2 
 
-## Write a short comment describing this function
+##
+## makeCacheMatrix  is the function created
+## setinverse  will store the inverse of the matrix
+## getinverse  will reterive the inverse of the matrix
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) { 
+  
+  i  <- NULL 
+  
+  set  <- function(y){ 
+    x <<- y 
+    i <<- NULL  
+  } 
+  
+  get  <- function() x 
+  
+  setinverse  <- function(inverse) 
+    i  <<- inverse 
+  
+  getinverse  <- function() i  
+  
+  list(set= set, get = get,  
+       setinverse = setinverse,  
+       getinverse = getinverse)   
+} 
 
-}
+## Cachesolve will prepares  the inverse of the matrix.
+## solve will determine the inverse of the matrix
+## getinverse() will get inverse matrix from the cache.
+
+cacheSolve <- function(x, ...) {  
+  i  <- x$getinverse()  
+  
+  if (!is.null(i)){  
+    message("getting cached data")  
+    return(i)  
+  } 
+  
+  data  <- x$get()  
+  i  <- solve(data, ...)  
+  x$setinverse(i)  
+  i 
+  
+} 
+
+### How to execute this Program :: steps to be followed.
+
+### m1 = matrix(c(1,2,3,4),nrow=2,ncol=2)
+### > m2 <- makeCacheMatrix(m1)
+###
+### > m2$get()
+### [,1] [,2]
+### [1,]    1    3
+### [2,]    2    4
+###
+### > m2$getinverse()
+### NULL
+###
+### > m3 <- cacheSolve(m2)
+###
+### > m2$getinverse()
+### [,1] [,2]
+### [1,]   -2  1.5
+### [2,]    1 -0.5
+###
+### > cacheSolve(m2)
+### getting cached data
+### [,1] [,2]
+### [1,]   -2  1.5
+### [2,]    1 -0.5
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
